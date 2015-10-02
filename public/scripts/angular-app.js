f0efde5eb85bfb41c4ab9120f4bc0062428e4434
David Hayes <davidlynnhayes@att.net>
2015-10-02 14:25:05 -0500

    full stack in place mongo to angular

--- public/scripts/angular-app.js
+++ public/scripts/angular-app.js
@@ -3,9 +3,14 @@ var app = angular.module('warp', ['ngRoute']);
 app.controller('BoardCtrl', ['$scope', '$route', '$http', function($scope, $route, $http) {
   $http.get('/tokens').success(function(data) {
     console.log('fetched data');
-    console.log(data);
-  })
-  $scope.spaces = tokens;
+    for (key in data) {
+      console.log(data[key].row, data[key].col, data[key].tokenId);
+    }
+
+  $scope.spaces = data;
+
+
+
   $scope.sortCol = 'col';
   $scope.sortRow = 'row';
   $scope.switchIt = function(fsw) {
@@ -18,165 +23,168 @@ app.controller('BoardCtrl', ['$scope', '$route', '$http', function($scope, $rout
     };
     $route.reload();
   };
-  $scope.tellMe = function(col, row, id) {
-    console.log(col, row, id);
+  $scope.tellMe = function(col, row, tokenId) {
+    console.log(col, row, tokenId);
   };
+
+});
 
 }]);
 
 
-var tokens = [
-  {
-    row: 1,
-    col: 1,
-    id: 'r1'
-  },
-  {
-    row: 2,
-    col: 1,
-    id: 'r2'
-  },
-  {
-    row: 3,
-    col: 1,
-    id: 'r3'
-  },
-  {
-    row: 1,
-    col: 2,
-    id: 'r4'
-  },
-  {
-    row: 2,
-    col: 2,
-    id: 'r5'
-  },
-  {
-    row: 3,
-    col: 2,
-    id: 'b1'
-  },
-  {
-    row: 1,
-    col: 3,
-    id: 'b2'
-  },
-  {
-    row: 2,
-    col: 3,
-    id: 'b3'
-  },
-  {
-    row: 3,
-    col: 3,
-    id: 'b4'
-  },
-  {
-    row: 1,
-    col: 4,
-    id: 'b5'
-  },
-  {
-    row: 2,
-    col: 4,
-    id: 'b6'
-  },
-  {
-    row: 3,
-    col: 4,
-    id: 'b7'
-  },
-  {
-    row: 1,
-    col: 5,
-    id: 'r2'
-  },
-  {
-    row: 2,
-    col: 5,
-    id: 'r3'
-  },
-  {
-    row: 3,
-    col: 5,
-    id: 'b1'
-  },
-  {
-    row: 1,
-    col: 6,
-    id: 'b2'
-  },
-  {
-    row: 2,
-    col: 6,
-    id: 'b3'
-  },
-  {
-    row: 3,
-    col: 6,
-    id: 'r4'
-  },
-  {
-    row: 1,
-    col: 7,
-    id: 'b4'
-  },
-  {
-    row: 2,
-    col: 7,
-    id: 'b9'
-  },
-  {
-    row: 3,
-    col: 7,
-    id: 'b9'
-  },
-  {
-    row: 1,
-    col: 8,
-    id: 'r1'
-  },
-  {
-    row: 2,
-    col: 8,
-    id: 'r2'
-  },
-  {
-    row: 3,
-    col: 8,
-    id: 'r3'
-  },
-  {
-    row: 1,
-    col: 9,
-    id: 'b1'
-  },
-  {
-    row: 2,
-    col: 9,
-    id: 'b2'
-  },
-  {
-    row: 3,
-    col: 9,
-    id: 'b3'
-  },
-  {
-    row: 1,
-    col: 10,
-    id: 'r4'
-  },
-  {
-    row: 2,
-    col: 10,
-    id: 'b4'
-  },
-  {
-    row: 3,
-    col: 10,
-    id: 'b9'
-  }
-];
-// for (var key in tokens) {
-// console.log(key, tokens[key]);
-// }
+// var tokens = [
+//   {
+//     row: 1,
+//     col: 1,
+//     tokenId: 'r1'
+//   },
+//   {
+//     row: 2,
+//     col: 1,
+//     tokenId: 'r2'
+//   },
+//   {
+//     row: 3,
+//     col: 1,
+//     tokenId: 'r3'
+//   },
+//   {
+//     row: 1,
+//     col: 2,
+//     tokenId: 'r4'
+//   },
+//   {
+//     row: 2,
+//     col: 2,
+//     tokenId: 'r5'
+//   },
+//   {
+//     row: 3,
+//     col: 2,
+//     tokenId: 'b1'
+//   },
+//   {
+//     row: 1,
+//     col: 3,
+//     tokenId: 'b2'
+//   },
+//   {
+//     row: 2,
+//     col: 3,
+//     tokenId: 'b3'
+//   },
+//   {
+//     row: 3,
+//     col: 3,
+//     tokenId: 'b4'
+//   },
+//   {
+//     row: 1,
+//     col: 4,
+//     tokenId: 'b5'
+//   },
+//   {
+//     row: 2,
+//     col: 4,
+//     tokenId: 'b6'
+//   },
+//   {
+//     row: 3,
+//     col: 4,
+//     tokenId: 'b7'
+//   },
+//   {
+//     row: 1,
+//     col: 5,
+//     tokenId: 'r2'
+//   },
+//   {
+//     row: 2,
+//     col: 5,
+//     tokenId: 'r3'
+//   },
+//   {
+//     row: 3,
+//     col: 5,
+//     tokenId: 'b1'
+//   },
+//   {
+//     row: 1,
+//     col: 6,
+//     tokenId: 'b2'
+//   },
+//   {
+//     row: 2,
+//     col: 6,
+//     tokenId: 'b3'
+//   },
+//   {
+//     row: 3,
+//     col: 6,
+//     tokenId: 'r4'
+//   },
+//   {
+//     row: 1,
+//     col: 7,
+//     tokenId: 'b4'
+//   },
+//   {
+//     row: 2,
+//     col: 7,
+//     tokenId: 'b9'
+//   },
+//   {
+//     row: 3,
+//     col: 7,
+//     tokenId: 'b9'
+//   },
+//   {
+//     row: 1,
+//     col: 8,
+//     tokenId: 'r1'
+//   },
+//   {
+//     row: 2,
+//     col: 8,
+//     tokenId: 'r2'
+//   },
+//   {
+//     row: 3,
+//     col: 8,
+//     tokenId: 'r3'
+//   },
+//   {
+//     row: 1,
+//     col: 9,
+//     tokenId: 'b1'
+//   },
+//   {
+//     row: 2,
+//     col: 9,
+//     tokenId: 'b2'
+//   },
+//   {
+//     row: 3,
+//     col: 9,
+//     tokenId: 'b3'
+//   },
+//   {
+//     row: 1,
+//     col: 10,
+//     tokenId: 'r4'
+//   },
+//   {
+//     row: 2,
+//     col: 10,
+//     tokenId: 'b4'
+//   },
+//   {
+//     row: 3,
+//     col: 10,
+//     tokenId: 'b9'
+//   }
+// ];
+// // for (var key in tokens) {
+// // console.log(key, tokens[key]);
+// // }
+// console.log(tokens);
